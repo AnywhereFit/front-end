87bdc81d35c9232211a5cd93b5f782c3668e5ab6
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-20 16:48:49 -0800

    basis login/signup page

--- anywhere-fit/src/components/SignUp.js
+++ anywhere-fit/src/components/SignUp.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import axios from 'axios';
 
 const SignUp = () => {
-    return(
-        <div>
-            <h2>Sign Up</h2>
-        </div>
-    )
+    const { push } = useNavigate();
+
+    const [cred, setCred] = useState({
+        username: "",
+        password: "",
+        authCode: ""
+    });
+
+    const handleChange = (e) => {
+        setCred({
+            ...cred,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('', cred)
+            .then(resp => {
+                localStorage.setItem("token", );
+                push('/');
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+
+    return (
+    <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input onChange={handleChange} name="username" id="username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input onChange={handleChange} name="password" type="password" id="password" />
+          </div>
+          <div>
+            <label htmlFor="authcode">Auth Code:</label>
+            <input onChange={handleChange} name="authcode" id="authcode" />
+          </div>
+          <button>Submit</button>
+        </form>
+    </div>)
 }
 
 export default SignUp;
