64a6b9c74a0526a1f895bb4d88c263ed55791b56
datboiluiskrrt <datboiluiskrrt@gmail.com>
2021-12-20 18:35:59 -0600

    Creating the Landing Page for the Instructor

--- anywhere-fit/src/App.js
+++ anywhere-fit/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './App.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+import InsLanding from "./components/InsLanding";
 function App() {
   return (
-      <div className="App">
-        <header>
-          <h1>Anywhere Fitness</h1>
-          <Link to="login">Login</Link>
-          <Link to="signup">Sign Up</Link>
-        </header>
-      </div>
+    <div className="App">
+      <header>
+        <h1>Anywhere Fitness</h1>
+        <Link to="login">Login</Link>
+        <Link to="signup">Sign Up</Link>
+      </header>
+      <InsLanding />
+    </div>
   );
 }
 
