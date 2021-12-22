d52ef5cab9b0e16e462e1e0e16fbaea010b4adfb
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-22 12:06:02 -0800

    put routes into app instead of index

--- anywhere-fit/src/App.js
+++ anywhere-fit/src/App.js
@@ -1,16 +1,54 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import NavBar from "./components/NavBar";
 import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import CreateClass from "./components/createClass/CreateClass";
+import InsLanding from "./components/InsLanding";
+import Client from "./components/Client";
+
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1>Anywhere Fitness</h1>
-        <Login />
-      </header>
-    </div>
+    <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="login" element={<Login />} />
+      <Route exact path="signup" element={<SignUp />} />
+
+      <Route 
+        path="createclass" 
+        element={
+          <PrivateRoute>
+            <CreateClass />
+          </PrivateRoute>
+        }
+      />
+      
+      <Route 
+        path="inslanding" 
+        element={
+          <PrivateRoute>
+            <InsLanding />
+          </PrivateRoute>
+        } 
+      />
+
+      <Route 
+        path="client" 
+        element={
+          <PrivateRoute>
+            <Client />
+          </PrivateRoute>
+        } 
+      />
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
