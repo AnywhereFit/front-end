e84833bfe69607f4b9b65547657c976636a8830a
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-22 14:26:49 -0800

    deleting components

--- anywhere-fit/src/Components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-    const auth = localStorage.getItem('token');
-    return auth ? children : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
